5067998c26de0c3055078b9b2ca0a579f840fe7f
Zhong, keli <qzk@126.com>
2017-03-08 17:48:38 +0800

    add metric

--- metric/metric.py
+++ metric/metric.py
@@ -1,28 +1,27 @@
 import numpy as np
-from utils.data_utils import read_data, sentence2id, load_vocab
-vocab = load_vocab('./data/vocabulary/vocab.pkl')
-def MakeRevertVocab(vocab):
-    dic = {}
-    for k, v in vocab.items():
-        dic[v] = k
-    return dic
-vocab = MakeRevertVocab(vocab)
-i = 0
-# Evaluation
+
+ignore_label = 0
+
+
 def Perplexity(label, pred):
     label = label.T.reshape((-1,))
     loss = 0.
-    mask_count = 0
-    str = ''
+    num = 0.
     for i in range(pred.shape[0]):
-        if int(label[i]) == 0:
-            mask_count += 1
-            continue
+        if int(label[i]) != ignore_label:
+            num += 1
+            loss += -np.log(max(1e-10, pred[i][int(label[i])]))
+    return loss / num
 
-        str = str + " " + vocab.get(int(label[i]), 'unk')
-        loss += -np.log(max(1e-10, pred[i][int(label[i])]))
-    ++i
-    if i % 200 == 0:
-        print(str+'\n')
-    return np.exp(loss / (label.size - mask_count))
 
+def customized_metric(label, pred):
+    label = label.T.reshape((-1,))
+    pred_indices = np.argmax(pred, axis=1)
+    num = 0
+    for i in range(pred.shape[0]):
+        if int(label[i]) != ignore_label:
+            l = sum(map(int, str(int(label[i] - 3)))) if label[i] >= 3 else 0
+            p = sum(map(int, str(int(pred_indices[i] - 3)))) if pred_indices[i] >= 3 else 0
+            if l == p:
+                num += 1
+    return float(num) / pred.shape[0] * 100
