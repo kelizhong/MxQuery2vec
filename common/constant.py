7305366dc46555133cdb655ac242274ad8406b94
Zhong <keliz@f45c89a84d5b.ant.amazon.com>
2017-04-09 21:40:50 +0800

    add commnet

--- common/constant.py
+++ common/constant.py
@@ -1,8 +1,8 @@
 # coding: utf-8
-#pylint: disable=invalid-name
+# pylint: disable=invalid-name
 """constant value"""
 bos_word = '<s>'  # begin of sentence
 eos_word = '</s>'  # end of sentence
 unk_word = '<unk>'  # unknown word
-pad_word = '<pad>'  # unknown word
-special_words = {pad_word: 0, unk_word: 1, bos_word: 2, eos_word: 3}  # word: index
+pad_word = '<pad>'  # pad word
+special_words = {pad_word: 0, unk_word: 1, bos_word: 2, eos_word: 3}  # special word: index
